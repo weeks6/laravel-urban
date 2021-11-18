9edbe6f8bcf04d0a4b4cfd4c8b5a103fe9410367
weeks6 <bridcarp1@gmail.com>
2021-11-18 15:36:01 +0300

    fixes

--- public/js/app.js
+++ public/js/app.js
@@ -19794,8 +19794,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\OSPanel\domains\laravel-urban\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\OSPanel\domains\laravel-urban\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\OpenServer\domains\laravel-urban\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\OpenServer\domains\laravel-urban\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
